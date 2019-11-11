ecca088a2d4d9f7d2e11fb9a5ecdd3b9a219481f
Anthony Ferraioli <amf7619@g.rit.edu>
2019-11-11 12:11:41 -0500

    Player Movement Started

---
+++ OnTheSpot3/Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private PlayerMovement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement.MoveDirection(PlayerMovement.movementDirection.Left);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement.MoveDirection(PlayerMovement.movementDirection.Right);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement.MoveDirection(PlayerMovement.movementDirection.Forward);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement.MoveDirection(PlayerMovement.movementDirection.Backward);
+        }
+    }
+}
---
+++ OnTheSpot3/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    private Vector3 direction;
+
+    private Rigidbody myRigidBody;
+
+    public enum movementDirection
+    {
+        Forward, Backward, Left, Right
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(direction != new Vector3(0.0f, 0.0f, 0.0f))
+            MoveCharacter();
+        KeepUpright();
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.AddForce(direction * movementSpeed);
+    }
+
+    public void MoveDirection(movementDirection e)
+    {
+        switch(e)
+        {
+            case movementDirection.Forward:
+                direction = new Vector3(0.0f, 0.0f, 1.0f);
+                break;
+            case movementDirection.Backward:
+                direction = new Vector3(0.0f, 0.0f, -1.0f);
+                break;
+            case movementDirection.Left:
+                direction = new Vector3(-1.0f, 0.0f, 0.0f);
+                break;
+            case movementDirection.Right:
+                direction = new Vector3(1.0f, 0.0f, 1.0f);
+                break;
+            default:
+                direction = new Vector3(0.0f, 0.0f);
+                break;
+        }
+    }
+
+    void KeepUpright()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+}
