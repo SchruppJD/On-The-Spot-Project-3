7e5a665cd5f4eb536808e0ac0b092f4845017764
Maliik <mab8141@g.rit.edu>
2019-11-13 21:46:43 -0500

    Creation of victory Scene

---
+++ OnTheSpot3/Assets/Scripts/Victory/Tallying.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tallying : MonoBehaviour
+{
+    public GameObject scoreSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreSystem would be set here
+        //scoreSystem = GameObject.FindGameObjectWithTag("");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
