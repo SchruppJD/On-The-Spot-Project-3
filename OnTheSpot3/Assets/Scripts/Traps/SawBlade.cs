33c535fd4d49fa08eaf6b230c3123a7c949f215e
TJ Driscoll <tjdriscoll4@gmail.com>
2019-11-16 17:37:28 -0500

    Made the sawblade trap, testing area for traps, and base abstract class for all traps to use.

---
+++ OnTheSpot3/Assets/Scripts/Traps/SawBlade.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : Trap
+{
+    public GameObject leftBlade;
+    public GameObject rightBlade;
+
+    Collider detectionArea;
+
+    Animator animator;
+
+    private new void Start()
+    {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentState = TrapState.Active;
+        triggerChance = .5f;
+        detectionArea = GetComponent<BoxCollider>();
+        reactivateTime = 0.5f;
+        animator = GetComponent<Animator>();
+    }
+
+    protected override bool Activate()
+    {
+        if (detectionArea.bounds.Intersects(player.GetComponent<Collider>().bounds))
+        {
+            if (Random.Range(0, 1f) > triggerChance)
+            {
+                animator.SetBool("Trigger", true);
+                return true;
+            }
+            else
+            {
+                currentState = TrapState.Reactivate;
+                return false;
+            }
+        }
+        return false;
+    }
+
+    protected override bool Reset()
+    {
+        return false;
+    }
+
+    protected override bool Trigger()
+    {
+        return false;
+    }
+}
---
+++ OnTheSpot3/Assets/Scripts/Traps/Trap.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Trap : MonoBehaviour
+{
+    protected enum TrapState
+    {
+        Active,
+        Reactivate,
+        Triggered,
+        Deactive
+    }
+
+    protected float triggerChance;
+    protected TrapState currentState;
+    protected GameObject player;
+    protected float reactivateTime;
+    float currentTime = 0;
+
+    protected void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentState = TrapState.Active;
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case TrapState.Active:
+                if(Activate())
+                {
+                    currentState = TrapState.Triggered;
+                }
+                break;
+
+            case TrapState.Reactivate:
+                if(Reactivate())
+                {
+                    currentState = TrapState.Active;
+                }
+                break;
+
+            case TrapState.Triggered:
+                if(Trigger())
+                {
+                    currentState = TrapState.Deactive;
+                }
+                break;
+
+            case TrapState.Deactive:
+                if(Reset())
+                {
+                    currentState = TrapState.Active;
+                }
+                break;
+        }
+    }
+
+    protected abstract bool Activate();
+    protected abstract bool Trigger();
+    protected abstract bool Reset();
+
+    bool Reactivate()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > reactivateTime)
+        {
+            currentTime = 0;
+            return true;
+        }
+        return false;
+    }
+}
