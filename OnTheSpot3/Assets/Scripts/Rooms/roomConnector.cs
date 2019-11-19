b9b87535b508171652f209273e2f762e343d38a7
James Schrupp <jds5583@rit.edu>
2019-11-19 04:53:52 -0500

    Room Modules

---
+++ OnTheSpot3/Assets/Scripts/Rooms/roomConnector.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomConnector : MonoBehaviour
+{
+
+    bool active;
+
+    roomManager GameController;
+    GameObject[] players;
+    int alivePlayers;
+    List<GameObject> donePlayers;
+    List<GameObject> playersInZone;
+
+    GameObject rightEdge;
+    BoxCollider rightWall;
+    GameObject leftEdge;
+    BoxCollider leftWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = true;
+        GameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<roomManager>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+        //Debug.Log("Players: " + players.Length);
+        donePlayers = new List<GameObject>();
+        playersInZone = new List<GameObject>();
+        alivePlayers = players.Length;
+
+        rightEdge = transform.Find("RightEdge").gameObject;
+        rightWall = rightEdge.transform.GetChild(0).GetComponentInChildren<BoxCollider>();
+        leftEdge = transform.Find("LeftEdge").gameObject;
+        leftWall = leftEdge.transform.GetChild(0).GetComponent<BoxCollider>();
+
+        openLeft();
+        sealRight();
+
+        InvokeRepeating("checkAlivePlayers", 2.0f, 5f);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void checkAlivePlayers()
+    {
+        if (active)
+        {
+            alivePlayers = players.Length;
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].transform.position.y < 0)
+                {
+                    alivePlayers--;
+                }
+            }
+
+            if (alivePlayers == 0)
+            {
+                roomFinished();
+            }
+
+             
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            
+
+            bool alreadyFinished = false;
+            foreach (var player in donePlayers)
+            {
+                if (other.gameObject == player)
+                {
+                    alreadyFinished = true;
+                }
+            }
+
+            if (alreadyFinished == false)
+            {
+                donePlayers.Add(other.gameObject);
+                if(donePlayers.Count == 1)
+                {
+                    leftEdge.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+                }
+            }
+
+            playersInZone.Add(other.gameObject);
+
+            if (playersInZone.Count == alivePlayers)
+            {
+                roomFinished();
+            }
+
+            checkAlivePlayers();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playersInZone.Remove(other.gameObject);
+        if(active == false && playersInZone.Count == 0)
+        {
+            sealRight();
+        }
+    }
+
+    void roomFinished()
+    {
+        active = false;
+        sealLeft();
+        openRight();
+
+        for (int i = 0; i < donePlayers.Count; i++)
+        {
+            GameController.playerFinished(donePlayers[i]);
+        }
+
+        GameController.changeRoom();
+
+        
+    }
+
+    void openRight()
+    {
+        rightEdge.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        rightWall.isTrigger = true;
+    }
+
+    void openLeft()
+    {
+        leftEdge.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        leftWall.isTrigger = true;
+    }
+
+    void sealRight()
+    {
+        rightEdge.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        rightWall.isTrigger = false;
+    }
+
+    void sealLeft()
+    {
+        leftEdge.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        leftWall.isTrigger = false;
+    }
+
+    public void Reset()
+    {
+        roomFinished();
+    }
+}
---
+++ OnTheSpot3/Assets/Scripts/Rooms/roomManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomManager : MonoBehaviour
+{
+    public GameObject[] players;
+    public GameObject camera;
+
+    public GameObject startRoom;
+    float startRoomWidth;
+    public GameObject endRoom;
+    float endRoomWidth;
+    public GameObject connectorRoom;
+    float connectorRoomWidth;
+    public GameObject[] roomPrefabs;
+
+    int roomIndex;
+    GameObject activeRoom;
+    float activeRoomWidth;
+    GameObject nextRoom;
+    float nextRoomWidth;
+
+    GameObject oldConnector;
+    GameObject activeConnector;
+    GameObject nextConnector;
+
+    int[] playerPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPoints = new int[players.Length];
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = Instantiate(players[i], new Vector3(0, 1, i), Quaternion.identity);
+            playerPoints[i] = 0;
+        }
+            
+        connectorRoomWidth = connectorRoom.GetComponent<BoxCollider>().size.x;
+        endRoomWidth = endRoom.GetComponent<BoxCollider>().size.x;
+        startRoomWidth = startRoom.GetComponent<BoxCollider>().size.x;
+
+        //Create StartRoom
+        GameObject start = Instantiate(startRoom, new Vector3(0, 0, 0), Quaternion.identity);
+        activeConnector = start;
+        roomIndex = Random.Range(0, roomPrefabs.Length);
+        activeRoom = roomPrefabs[roomIndex];
+        activeRoomWidth = activeRoom.GetComponent<BoxCollider>().size.x;
+        activeRoom = Instantiate(activeRoom, new Vector3((startRoomWidth/2) + (activeRoomWidth/2),0,0), Quaternion.identity);
+
+        nextConnector = Instantiate(connectorRoom, new Vector3((activeRoom.transform.position.x) + (activeRoomWidth / 2) + (connectorRoomWidth / 2), 0, 0), Quaternion.identity);
+
+        updateCamera();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            print("R key was pressed");
+            nextConnector.GetComponent<roomConnector>().Reset();
+        }
+    }
+
+
+    void newRoom(){
+        int newRoomIndex;
+        do
+        {
+            newRoomIndex = Random.Range(0, roomPrefabs.Length);
+        } while (roomIndex == newRoomIndex);
+        roomIndex = newRoomIndex;
+        nextRoom = roomPrefabs[roomIndex];
+        nextRoomWidth = nextRoom.GetComponent<BoxCollider>().size.x;
+        float newPositionX = activeRoom.transform.position.x + (activeRoomWidth/2) + connectorRoomWidth + (nextRoomWidth/2);
+        //Debug.Log(connectorRoom.GetComponent<BoxCollider>().size.x);
+        nextRoom = Instantiate(nextRoom, new Vector3(newPositionX, 0, 0), Quaternion.identity);
+        Destroy(activeRoom, 0.1f);
+        
+        activeRoom = nextRoom;
+
+        newConnector();
+    }
+
+    void newConnector(){
+        Destroy(activeConnector, 1f);
+        activeConnector = nextConnector;
+        nextConnector = Instantiate(connectorRoom, new Vector3( (activeRoom.transform.position.x) + (activeRoomWidth/2) + (connectorRoomWidth/2)      ,0,0), Quaternion.identity);
+    }
+
+    void respawnPlayers(){
+        for (int i = 0; i < players.Length; i++)
+        {
+            //Debug.Log(players.Length);
+            //Debug.Log(players[i].transform.position.y);
+            //if(players[i].transform.position.y < 0)
+            //{
+                players[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                players[i].transform.position = new Vector3(activeConnector.transform.position.x, 1.5f, (activeConnector.transform.position.z - 5) + (3*i)  );
+            //}
+        }
+    }
+     
+    void updateCamera(){
+        //Debug.Log(activeRoom.transform.position.x);
+        camera.transform.position = new Vector3(activeRoom.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+    }
+
+    public void playerFinished(GameObject player)
+    {
+        for (int i = 0; i < playerPoints.Length; i++)
+        {
+            if(player == players[i])
+            {
+                playerPoints[i]++;
+                
+            }
+        }
+    }
+
+    public void changeRoom()
+    {
+        newRoom();
+        updateCamera();
+        respawnPlayers();
+
+        Debug.Log("Current Points: ");
+        for (int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player" + (i + 1) + ": " + playerPoints[i]);
+        }
+    }
+
+}
